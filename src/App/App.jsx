0842861b229c211d8eebddf3e9e35d330fd31e88
Ivan Klimovskiy <klimoffsky27@gmail.com>
2024-06-24 11:49:53 +0300

    Исправить цифру 20 на 21

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -105,7 +105,7 @@ const App = () => {
             <option value="18">18</option>
             <option value="19">19</option>
             <option value="20">20</option>
-            <option value="21">20</option>
+            <option value="21">21</option>
           </Form.Select>
         </Form.Group>
         <Button
